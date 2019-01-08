a99ab9ae774c97ba2b67af84b7961d7f2f3b13e3
eselbeus <eselbeus@gmail.com>
2019-01-08 08:48:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,14 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  new_arr = array.sort
+  return new_arr
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
